98a2bf56e243d309de3144de0f2b78a28a4745b5
kelly0924xu <kelly0924xu@gmail.com>
2021-10-17 19:51:34 -0500

    step a

---
+++ midQ4.R
@@ -0,0 +1,13 @@
+#import dataset
+library(readr)
+BRFSS  = read_csv(file = 'https://raw.githubusercontent.com/kijohnson/ADA-Fall-2021/master/BRFSS2017_10percent_v.csv')
+
+#get median of height by sex
+with(BRFSS, by(ht_meters, SEX, median, na.rm=T))
+
+#use plot to show median of height by sex
+library(ggplot2)
+ggplot(BRFSS, aes(x=SEX, y=ht_meters)) +
+  geom_boxplot(aes(group = SEX)) +
+  scale_x_continuous(breaks = c(1,2), limits = c(0,3), labels=c('Male',"Female")) +
+  ylab('Height (meters)')
